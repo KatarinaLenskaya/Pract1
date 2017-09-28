c2cd505a43379ac6dfe8fb57b919ddfd4fbd84f1
KatarinaLenskaya <katarina.lenskaya@gmail.com>
2017-09-28 16:26:49 +0300

    sort method implemented

--- src/com/catlerina/Main.java
+++ src/com/catlerina/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Have a nice day!");
+        sort(args);
         print(args);
     }
 
@@ -11,6 +12,20 @@ public class Main {
         if (args != null) {
             for (int i = 0; i < args.length; i++) {
                 System.out.println(args[i]);
+            }
+        }
+    }
+
+    public static void sort(String[] args){
+        if(args != null && args.length>0){
+            for (int i = args.length - 1; i > 0; i--) {//this loop help us not to compare those elements we have already sorted
+                for (int j = 0; j < i; j++){
+                    if(args[j].compareTo(args[j+1]) > 0){
+                        String temporary = args[j];
+                        args[j] = args[j+1];
+                        args[j+1] = temporary;
+                    }
+                }
             }
         }
     }
